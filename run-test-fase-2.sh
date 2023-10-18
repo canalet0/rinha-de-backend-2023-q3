1795ec95ca5b8eb4fe93a1b48340af419bd2004f
canalet0 <roc0canal@gmail.com>
2023-10-18 14:11:33 -0300

    Scripts de teste

---
+++ run-test-fase-2.sh
@@ -0,0 +1,12 @@
+WORKSPACE=$(pwd)
+GATLING_BIN_DIR="${WORKSPACE}/deps/gatling/bin"
+
+sh $GATLING_BIN_DIR/gatling.sh -rm local -s RinhaBackendSimulation \
+    -rd "DESCRICAO" \
+    -rf $WORKSPACE/results \
+    -sf $WORKSPACE/stress-test/user-files/simulations \
+    -rsf $WORKSPACE/resources \
+
+sleep 3
+
+curl -v "http://localhost:9999/contagem-pessoas"
